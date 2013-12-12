e9fd364dfc251035bfe78ccc2b397159fe67d761
Amy Slagle <slagle@ehatek.org>
2013-12-12 15:13:55 -0500

    Edit layout and move tabs into dash package.

--- packages/dash/package.js
+++ packages/dash/package.js
@@ -1,7 +1,9 @@
 Package.on_use(function (api) {
-	api.use(['templating'], 'client');
+	api.use(['templating', 'bootstrap-3', 'jqueryui'], 'client');
 
 	api.add_files("lib/test.js", "client");
+	api.add_files("lib/tabs.html", "client");
+	api.add_files("lib/tabs.js", "client");
 	api.add_files("lib/dash.html", "client");
 	api.add_files("lib/dash.js", "client");
 });
