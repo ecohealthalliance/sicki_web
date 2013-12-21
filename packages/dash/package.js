d4a603c99a157ed9467feaeef03f6446e59df703
Nico Preston <nicopresto@gmail.com>
2013-12-21 14:03:44 -0500

    adds EID ribbon top left

--- packages/dash/package.js
+++ packages/dash/package.js
@@ -5,10 +5,11 @@ Package.on_use(function (api) {
 	api.add_files("lib/tabs.html", "client");
 	api.add_files("lib/tabs.js", "client");
 	api.add_files("lib/dash.html", "client");
+	api.add_files("lib/dash.css", "client");
+	api.add_files("lib/dash.js", "client");
+	api.add_files("lib/map.html", "client");
 	api.add_files("lib/map.css", "client");
 	api.add_files("lib/map.js", "client");
 	api.add_files("lib/soma.png", "client");
 	api.add_files("lib/timer.png", "client");
-	api.add_files("lib/dash.css", "client");
-	api.add_files("lib/map.html", "client");
 });
