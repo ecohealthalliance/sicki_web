e339fca1d5dfeb2f8a6c824c4d7c8686c8268bcd
Nico Preston <nicopresto@gmail.com>
2014-01-07 21:59:40 -0500

    experimented with setting datamate config variables

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,3 +1,16 @@
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY'
 });
+
+//Stub for future datamate config settings
+
+/*
+Datamate.config.key({
+  keyField: 'qid'
+});
+
+Datamate.config.listfields ({
+	listFields: ['eidID', 'eventName','disease']
+});
+
+*/
