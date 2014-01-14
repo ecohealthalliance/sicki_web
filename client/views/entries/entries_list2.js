c96064211d3bd935c529b9b97c3702a00b3fcd9c
Nico Preston <nicopresto@gmail.com>
2014-01-13 20:35:40 -0500

    replaces entry list with reactive-table

--- client/views/entries/entries_list2.js
+++ client/views/entries/entries_list2.js
@@ -1,5 +1,20 @@
 Template.entriesList2.helpers({
   entries: function() {
-    return Entries.find();
+    /*return Entries.find();*/
+    return Entries;
+  },
+  fields: function() {
+    return [
+      {
+        key: 'eventName.val',
+        label: 'EventName'
+      }, {
+        key: 'dates.startDate.val',
+        label: 'Start Date'
+      }, {
+        key: 'dates.endDate.val',
+        label: 'End Date'
+      }
+    ];
   }
 });
