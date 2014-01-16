25a47a0c1b8eec80e7a258f12eaa33fd2a3123e7
Nico Preston <nicopresto@gmail.com>
2014-01-15 21:20:08 -0500

    adds api for references

---
+++ server/api.js
@@ -0,0 +1,7 @@
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    collectionApi = new CollectionAPI({ authToken: '97f0ad9e24ca5e0408a269748d7fe0a0' });
+    collectionApi.addCollection(References, 'references');
+    collectionApi.start();
+  });
+}
